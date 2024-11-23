692100b3bab5de879d691d5eec40acddd849bf73
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-24 01:14:08 +0530

    test

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidr[count.index]
   availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
   tags = merge(
     var.common_tags,
     var.public_subnet_tags,
