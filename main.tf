09e14a040c6d1f9c6b42d53ae084e1b91840d949
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-24 22:21:48 +0530

    adding ansible scripts

--- main.tf
+++ main.tf
@@ -165,3 +165,5 @@ resource "aws_route_table_association" "database" {
   subnet_id      = element(aws_subnet.database[*].id,count.index)
   route_table_id = aws_route_table.database.id
 }
+
+# 2hours mg chesina mistake
